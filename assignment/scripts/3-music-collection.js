2560950f97793c53e935bfe5521c458cc7e5b76d
Brock Benson <112825218+brockjbenson@users.noreply.github.com>
2022-12-08 11:17:35 -0600

    Update 3-music-collection.js
    object and function created

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+let Collection = [];
+// Created empty array named collection
+function addToCollection ( title, artist, yearPublished ) {
+
+}
+//Adding funciton called addToCollection with three given parameters 
+
+const myObject = {
+    'title': '',
+    'artist': '',
+    'yearPublished': '',
+}
+// Added new object
+
