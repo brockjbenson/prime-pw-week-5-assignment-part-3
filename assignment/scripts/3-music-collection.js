108d371b27c9b5b13b50dd923f2e5ea2aa637c9c
Brock Benson <brockjbenson@outlook.com>
2022-12-10 14:35:02 -0600

    all done

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,7 @@
 let collection = [];
 // Created empty array named collection
 function addToCollection ( title, artist, yearPublished ) {
-    const album = {
+    let album = {
         'title': title,
         'artist': artist,
         'yearPublished': yearPublished,
@@ -25,7 +25,26 @@ console.log(collection);
 function showCollection( array ) {
     console.log(array);
     for (let i = 0; i < array.length; i++) {
-       
-    } 
+        let album = array[i];
+        console.log('TITLE', album.title, 'ARTIST', album.artist, 'published in', album.yearPublished );
+        // Logging the collection in TITLE, ARTIST, published in: format by adding string along with the given object
+}
 }
  console.log(showCollection(collection));
+
+ // Testing show collection function
+
+ function findByArtist( artists ) {
+    console.log( 'In byArtist:', artists);
+    const arr = [];
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i].artist === artists) {
+            arr.push(collection[i]);
+        }
+    }
+    return arr;
+ }
+ console.log(findByArtist('Johnson'));
+ console.log(findByArtist('Sherry'));
+
+ // Testing findByArtist by using a an actual listed artist and a non listed artist
